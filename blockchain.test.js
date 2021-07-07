eac1e588db892b33eb1e3bece99516ec271984bb
olmaga <oliver.ganz@gmail.com>
2021-07-08 00:00:30 +0200

    Add Blockchain

---
+++ blockchain.test.js
@@ -0,0 +1,57 @@
+const Blockchain = require('./blockchain');
+const Block = require('./block');
+
+describe('Blockchain', () => {
+    let blockchain, blockchain2;
+
+    beforeEach(() => {
+        blockchain = new Blockchain();
+        blockchain2 = new Blockchain();
+    });
+
+    it('starts with the genesis block', () => {
+        expect(blockchain.chain[0]).toEqual(Block.genesis());
+    });
+
+    it('adds a new block', () => {
+        const data = 'foo';
+        blockchain.addBlock(data);
+
+        expect(blockchain.chain[1].data).toEqual(data);
+    });
+
+    it('validates a valid chain', () => {
+        blockchain2.addBlock('foo');
+
+        expect(blockchain.isValidChain(blockchain2.chain)).toBe(true);
+    });
+
+    it('invalidates a chain with a corrupt genesis block', () => {
+        blockchain2.chain[0].data = 'bad-data';
+
+        expect(blockchain.isValidChain(blockchain2.chain)).toBe(false);
+    });
+
+    it('invalidates a corrupt chain', () => {
+        blockchain2.addBlock('foo');
+        blockchain2.chain[1].data = 'not-foo';
+
+        expect(blockchain.isValidChain(blockchain2.chain)).toBe(false);
+    });
+    
+    it('replaces the chain if valid and longer', () => {
+        blockchain2.addBlock('bar');
+
+        blockchain.replaceChain(blockchain2.chain);
+
+        expect(blockchain.chain).toEqual(blockchain2.chain);
+    });
+    
+    it('does not replaces the chain with a too short chain', () => {
+        blockchain.addBlock('foo');
+
+        blockchain.replaceChain(blockchain2.chain);
+
+        expect(blockchain.chain).not.toEqual(blockchain2.chain);
+    });
+})
