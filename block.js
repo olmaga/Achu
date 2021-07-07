db8d077abd7def4a7d50f40423177b3d315e7cb5
olmaga <oliver.ganz@gmail.com>
2021-07-07 22:46:39 +0200

    Created a Block

---
+++ block.js
@@ -0,0 +1,40 @@
+const SHA256 = require('crypto-js/sha256');
+
+class Block {
+    constructor(timestamp, lastHash, hash, data) {
+        this.timestamp = timestamp;
+        this.lastHash = lastHash;
+        this.hash = hash;
+        this.data = data;
+    }
+
+    toString() {
+        let time = ("" + new Date(this.timestamp));
+        if (time.includes("GMT")) {
+            time = time.substring(0, time.indexOf("GMT") - 1);
+        }
+        return `Block - 
+            Timestamp: ${time}
+            Last Hash: ${this.lastHash ? this.lastHash.substring(0, 10)+'...' : 'Genesis'}
+            Hash     : ${this.hash.substring(0, 10)}...
+            Data     : ${this.data}`;
+    }
+
+    static genesis() {
+        return new this('Genesis Time', '----------', '1337--1337', [])
+    }
+
+    static mineBlock(lastBlock, data) {
+        const timestamp = Date.now();
+        const lastHash = lastBlock.hash;
+        const hash = Block.hash(timestamp, lastHash, data);
+
+        return new Block(timestamp, lastHash, hash, data);
+    }
+
+    static hash(timestamp, lastHash, data) {
+        return SHA256(`${timestamp}${lastHash}${data}`).toString();
+    }
+}
+
+module.exports = Block;
