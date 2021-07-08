6ebdbb87f2cf979155d52d9016ed5b1a1d5a1555
olmaga <oliver.ganz@gmail.com>
2021-07-08 08:09:46 +0200

    Sync multiple instances via websockets

--- app/index.js
+++ app/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const Blockchain = require('../blockchain');
+const P2pServer = require('./p2p-server');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 // run it like this: `$ HTTP_PORT=3002 npm run dev`
 
 const app = express();
 const blockchain = new Blockchain();
+const p2pServer = new P2pServer(blockchain);
 
 app.use(express.json());
 
@@ -16,9 +18,13 @@ app.get('/blocks', (req, res) => {
 app.post('/mine', (req, res) => {
     const block = blockchain.addBlock(req.body.data);
     console.log(`New block added: ${block.toString()} - successfully mined.`);
+
+    p2pServer.syncChains();
+    
     res.redirect('/blocks');
 });
 
 app.listen(HTTP_PORT, () => {
     console.log(`Listening on port ${HTTP_PORT}`);
 })
+p2pServer.listen();
